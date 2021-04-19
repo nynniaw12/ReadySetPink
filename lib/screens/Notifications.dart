cc7bda9de31734134d3e79271cb8902c97808bea
Ege <egozdemir24@my.aci.k12.tr>
2021-04-19 14:16:15 +0300

    Added calendar.dart and Notifications.dart

--- lib/screens/Notifications.dart
+++ lib/screens/Notifications.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-class Notifications extends StatelessWidget {
+/*FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+// initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('app_icon');
+final IOSInitializationSettings initializationSettingsIOS = IOSInitializationSettings(
+    onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+final MacOSInitializationSettings initializationSettingsMacOS = MacOSInitializationSettings();
+final InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+    iOS: initializationSettingsIOS,
+    macOS: initializationSettingsMacOS);
+
+await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+onSelectNotification: selectNotification);
+
+async; class Notifications extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
@@ -9,4 +24,4 @@ class Notifications extends StatelessWidget {
 
 
 
-}
+}*/
